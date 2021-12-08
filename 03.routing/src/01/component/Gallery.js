ff2f2584ecffef3ee232301db4f08e210193bac6
naeunkim1227 <kne0140@daum.net>
2021-12-08 10:39:45 +0900

    router #1

--- 03.routing/src/01/component/Gallery.js
+++ 03.routing/src/01/component/Gallery.js
@@ -2,11 +2,10 @@ import React from 'react';
 
 export default function Gallery() {
     return (
-        <div>
-            <h1>Gallery</h1>
-            <a href='#/'>main</a>
-            <a href='#/gallery'>gallery</a>
-            <a href='#/guestbook'>guestbook</a>
-        </div>
+        <Layout>
+            <div>
+                <h1>Gallery</h1>
+            </div>
+        </Layout>
     );
 }
--- 03.routing/src/01/component/Main.js
+++ 03.routing/src/01/component/Main.js
@@ -2,11 +2,10 @@ import React from 'react';
 
 export default function Main() {
     return (
-        <div>
-            <h1>Main</h1>
-            <a href='#/'>main</a>
-            <a href='#/gallery'>gallery</a>
-            <a href='#/guestbook'>guestbook</a>
-        </div>
+        <Layout>
+            <div>
+                <h1>Main</h1>
+            </div>
+        </Layout>
     );
 }
